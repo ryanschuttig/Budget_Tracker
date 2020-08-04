cbd57e66377ded9e5e657a73449ae54fd3d97686
ryanschuttig <ryanschuttig@gmail.com>
2020-08-04 18:09:42 -0400

    added service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = ["/", "/index.html", "app.js", "favicon.ico"];
+const FILES_TO_CACHE = ["/", "/index.html", "index.js, db.js, styles.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
